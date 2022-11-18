8514650fc34f0848d341aa5e38b0f36f7c367f7b
CTT <gustavosilva90@gmail.com>
2022-11-18 16:34:45 -0300

    ESCADAS

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int n = 1;
+
+        while(n <= 5){
+            System.out.println(n);
+            n += 2;
+        }
+
+    }
+}
