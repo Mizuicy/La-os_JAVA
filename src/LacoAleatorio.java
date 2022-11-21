f79bac1360dc2470a4c42d7a20b2928ccee5a317
CTT <gustavosilva90@gmail.com>
2022-11-21 13:48:04 -0300

    LAÇOS 3 ATIVIDADE COMPLETAS

---
+++ src/LacoAleatorio.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class LacoAleatorio {
+    public static void main(String[] args) {
+        Random random = new Random(6);
+        Scanner scanner = new Scanner (System.in);
+
+
+    }
+}
